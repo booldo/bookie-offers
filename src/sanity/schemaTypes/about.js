0cdc6d6d4e0893b55839d10359effbbaef114043
brianstackKE <workhub254@outlook.com>
2025-09-11 13:38:51 +0300

    UI updates

--- src/sanity/schemaTypes/about.js
+++ src/sanity/schemaTypes/about.js
@@ -37,7 +37,7 @@ export default {
   preview: {
     select: {
       title: 'title',
-      country: 'country.country'
+      country: 'country->country'
     },
     prepare(selection) {
       const { title, country } = selection
